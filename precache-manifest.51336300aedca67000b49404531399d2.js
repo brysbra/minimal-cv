cf9649c3de90f974bf743ffd470c970b53f2e895
Brynjar Bragason <brynjar.bragason@k3btg.com>
2019-09-22 23:34:41 +0000

    Updates

---
+++ precache-manifest.51336300aedca67000b49404531399d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb342a53673e9ccc80783489513a9035",
+    "url": "/minimal-cv/index.html"
+  },
+  {
+    "revision": "68f9e9531ec853592b81",
+    "url": "/minimal-cv/static/css/main.1cdce6a4.chunk.css"
+  },
+  {
+    "revision": "9cb7bb1478ca8fdc46ed",
+    "url": "/minimal-cv/static/js/2.2e74004b.chunk.js"
+  },
+  {
+    "revision": "68f9e9531ec853592b81",
+    "url": "/minimal-cv/static/js/main.66078e70.chunk.js"
+  },
+  {
+    "revision": "d4b5da45dc71341eb4ac",
+    "url": "/minimal-cv/static/js/runtime-main.cdba7d76.js"
+  },
+  {
+    "revision": "7816b504a425f4a117ad319d3be1a519",
+    "url": "/minimal-cv/static/media/bsb.7816b504.jpg"
+  }
+]);
